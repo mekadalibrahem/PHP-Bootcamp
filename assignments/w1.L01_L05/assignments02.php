4c7046cf55f12076baa31a6b43829776bae33c28
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-07 20:18:37 +0300

    add week 01 assignment 02

---
+++ assignments/w1.L01_L05/assignments02.php
@@ -0,0 +1,12 @@
+<?php 
+
+/*
+
+    إستخدم التعليقات في كل من الأسطر التالية لمنع تشغيل ال Code
+    يجب إستخدام نوع مختلف من التعليقات مع كل سطر
+
+*/
+
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
