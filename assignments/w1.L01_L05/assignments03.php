965e619521f6fcc4229271707e5b5b28f621dbce
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-07 20:20:10 +0300

    add week 01 assignment 02

---
+++ assignments/w1.L01_L05/assignments03.php
@@ -0,0 +1,16 @@
+<?php 
+/* 
+
+    قم بتصليح التعليق التالي ليكون مناسب
+
+// My Application
+// Version 1.0
+// Created By Elzero
+
+*/
+
+/*
+ My Application
+ Version 1.0
+ Created By Elzero
+ */
