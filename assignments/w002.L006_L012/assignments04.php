9ecc67d2655e63c39413a397eab1bddcfc128cab
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-08 19:21:50 +0300

    add week02 assignment04

---
+++ assignments/w002.L006_L012/assignments04.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+/* 
+    التكليف 04
+
+    لديك ال Code التالي
+    يحتوي على \n وهي ال Character الخاص بال New Line
+    ولكن جميع هذه الكلمات سوف تظهر على نفس السطر
+    قم بالتعديل على السطر لجعل كل كلمة تظهر في سطر منفصلة عند الطباعة
+
+echo "We \n Love \n Elzero \n Web \n School";
+
+// Needed Output
+We
+Love
+Elzero
+Web
+School
+*/
+echo nl2br("We \n Love \n Elzero \n Web \n School");
