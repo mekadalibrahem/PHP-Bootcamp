c3f91e0fb4238b30e290d73124d4ed22a5a587c3
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-09 00:30:59 +0300

    add week03 assignment05

---
+++ assignments/w003.L013_L019/assignments05.php
@@ -0,0 +1,25 @@
+<?php 
+
+/*
+    التكليف 05
+
+    إنشيء Multiple Lines Comment
+    اكتب بداخله 10 كلمات من الكلمات المحجوزة في اللغة والتي لا يمكن إستخدمها في برمجياتك
+
+
+*/
+/*
+  10 Words Here :
+  public 
+  private 
+  protected 
+  function 
+  for 
+  if
+  switch 
+  while 
+  do 
+  class
+  
+*/
+
