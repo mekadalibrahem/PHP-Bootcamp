dbfbf026e21c57f3d012761ff3d90ef120d058e0
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-09 00:22:47 +0300

    add week03 assignment02

---
+++ assignments/w003.L013_L019/assignments02.php
@@ -0,0 +1,28 @@
+<?php 
+/* 
+    التكليف 02
+
+    لديك ال Code التالي يحتوي على متغيرين
+    المطلوب طباعة كلمة Web بخمس طرق مختلفة بواسطة ما تعلمته
+    الخمس طرق يكونوا بواسطة Echo فقط
+
+    $name = "elzero";
+    $$name = "Web";
+
+    //  not end this assignment 
+
+*/
+$name = "elzero";
+$$name = "Web";
+
+echo $$name ;
+echo "<br>" ;
+echo $elzero;
+
+echo "<br>" ;
+echo "${$name}" ;
+echo "<br>" ;
+echo "$elzero" ;
+echo "<br>" ;
+echo "web" ;
+echo "<br>" ;
