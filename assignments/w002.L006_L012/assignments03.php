67288617e5fe4e30eee946a7cf6bd5e1c4d3b9e1
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-08 19:16:31 +0300

    add week02 assignment03

---
+++ assignments/w002.L006_L012/assignments03.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+    التكليف 03
+
+    قم بإستبدال علامات الإستفهام
+    ضع مكانها ما تريد لتخرج بال Output المطلوب الموجود في الأسفل
+    يجب عدم إستخدام ال Heredoc ولا ال Nowdoc
+
+echo "???";
+
+// Needed Output
+// Hello "Elzero" \\ """ We Love "$$PHP"
+
+*/
+echo "Hello \"Elzero\" \\\\ \"\"\" We Love \"\$\$PHP\"";
+
+// Needed Output
+// Hello "Elzero" \\ """ We Love "$$PHP"
