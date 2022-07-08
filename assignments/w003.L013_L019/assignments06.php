f56846ef40dfef3bedd004608522f230f559b232
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-09 00:32:23 +0300

    add week03 assignment06

---
+++ assignments/w003.L013_L019/assignments06.php
@@ -0,0 +1,19 @@
+<?php 
+/* 
+    التكليف 06
+
+    في السطر الأول قم بكتابة Code يطبع رقم السطر الموجود فيه هذا ال Code
+    في السطر الثاني قم بطباعة إسم الملف الموجود فيه هذا ال Code
+    في السطر الثالث قم بطباعة ال Folder الذي يحتوي على الملف الذي تقوم بكتابة ال Code فيه
+
+"60"
+"C:\xampp\htdocs\elzero\index.php"
+"C:\xampp\htdocs\elzero"
+
+*/
+
+echo __LINE__ ;
+echo "<br>" ;
+echo __FILE__ ;
+echo "<br>" ;
+echo __DIR__ ;
