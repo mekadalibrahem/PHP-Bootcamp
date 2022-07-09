b147a9b243305ca12f8b3d369dbfc4256440b33b
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-07-09 17:47:39 +0300

    add week04 assignment05

---
+++ assignments/w004.L020_L029/assignments05.php
@@ -0,0 +1,30 @@
+<?php 
+    /* 
+        التكليف 05
+
+    لديك المتغير التالي والمطلوب طباعة القيم كما في المثال
+    في المكان المخصص يمكنك كتابة ال Code الخاص بك لتخرج القيم بشكل سليم
+    ممنوع تغيير قيمة المتغير ولا كتابة اي أرقام بيدك
+
+$points = 10;
+
+// Write Your Code Here
+
+echo $points; // 13
+
+// Write Your Code Here
+
+echo $points; // 8;
+    */
+    $points = 10;
+
+    $points += 3 ;
+
+
+echo $points; // 13
+echo "<br>" ;
+
+    $points  -= 5 ;
+
+echo $points; // 8;
+?>
