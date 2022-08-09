83b2831bf758d452299c58d8938871abb18d8fba
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-09 18:35:46 +0300

    add week07  assignment06

---
+++ assignments/w007.L053_L062/assignments06.php
@@ -0,0 +1,18 @@
+<?php 
+
+/* 
+التكليف التكليف 06
+
+    لديك ال Array التالية
+    بسطر واحد فقط قم بطباعة النتيجة المطلوبة من ال Array
+
+$chars = ["E", "l", "z", "e", "r", "o"];
+
+// Output
+"Elzero"
+*/
+
+
+$chars = ["E", "l", "z", "e", "r", "o"];
+
+echo join($chars) ;// Output "Elzero"
