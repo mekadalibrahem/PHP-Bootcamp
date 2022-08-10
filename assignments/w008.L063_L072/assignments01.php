e329325b837f1c2430cc3687c2562fc2b84be99c
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-11 00:25:56 +0300

    add week08 assignment 01

---
+++ assignments/w008.L063_L072/assignments01.php
@@ -0,0 +1,50 @@
+<?php 
+/* 
+التكليف التكليف 01
+
+    لديك ال Array التالية
+    المطلوب إستخدام ال Array Built-In Functions لتخرج بال Output المطلوب
+
+$friends = [
+  "AG" => "Ahmed Gamal",
+  "OM" => "Osama Mohamed",
+  "MG" => "Mahmoud Gamal",
+  "AS" => "Ahmed Samy",
+  "FA" => "Farid Ahmed",
+  "SM" => "Sayed Mohamed"
+];
+
+// Output
+Array
+(
+  [0] => Array
+    (
+      [ag] => Ahmed Gamal
+      [om] => Osama Mohamed
+    )
+  [1] => Array
+    (
+      [mg] => Mahmoud Gamal
+      [as] => Ahmed Samy
+    )
+  [2] => Array
+    (
+      [fa] => Farid Ahmed
+      [sm] => Sayed Mohamed
+    )
+)
+
+
+*/
+$friends = [
+    "AG" => "Ahmed Gamal",
+    "OM" => "Osama Mohamed",
+    "MG" => "Mahmoud Gamal",
+    "AS" => "Ahmed Samy",
+    "FA" => "Farid Ahmed",
+    "SM" => "Sayed Mohamed"
+  ];
+
+echo "<pre>" ;
+print_r(array_chunk($friends,2,true));
+echo "</pre>";
