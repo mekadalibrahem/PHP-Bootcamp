3a3f7cd13271c736a6e0f1f0afeec905b25b2e7c
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-11 11:55:24 +0300

    add week08 assignment 04

---
+++ assignments/w008.L063_L072/assignments04.php
@@ -0,0 +1,29 @@
+<?php 
+/* 
+التكليف 04
+
+    لديك ال Array التالية
+    المطلوب إستخدام ال Array Built-In Functions لتخرج بال Output المطلوب
+    يجب عمل الحل مرة بواسطة ال Array_Reduce مع إستخدام Anonymous Function
+    ثم عمله مرة أخرى بوسيلة أخرى
+
+$nums = [10, 20, 30];
+
+// Output
+60
+60
+*/
+
+
+$nums = [10, 20, 30];
+echo array_reduce($nums , fn( $c,$i) =>   $c + $i );
+echo "<br>";
+
+function sum(...$item){
+    $sum = 0 ;
+    foreach ($item as  $value) {
+       $sum +=  $value ; 
+    }
+    return $sum ;
+}
+echo sum(...$nums);
